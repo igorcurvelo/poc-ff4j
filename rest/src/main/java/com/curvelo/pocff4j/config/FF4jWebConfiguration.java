a7e2b0fe770b63530e970e029b3e71fe03f9c116
Igor Silva <igor.silva@luizalabs.com>
2021-11-02 11:37:53 -0300

    feat: rest project poc

---
+++ rest/src/main/java/com/curvelo/pocff4j/config/FF4jWebConfiguration.java
@@ -0,0 +1,32 @@
+package com.curvelo.pocff4j.config;
+
+import org.ff4j.FF4j;
+import org.ff4j.spring.boot.web.api.config.EnableFF4jSwagger;
+import org.ff4j.web.FF4jDispatcherServlet;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass({FF4jDispatcherServlet.class})
+@AutoConfigureAfter(FF4jConfig.class)
+public class FF4jWebConfiguration extends SpringBootServletInitializer {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public FF4jDispatcherServlet getFF4jDispatcherServlet(FF4j ff4j) {
+    FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
+    ff4jConsoleServlet.setFf4j(ff4j);
+    return ff4jConsoleServlet;
+  }
+
+  @Bean
+  public ServletRegistrationBean ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet) {
+    return new ServletRegistrationBean(ff4jDispatcherServlet, "/ff4j-web-console/*");
+  }
+
+}
